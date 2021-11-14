27af5dd00ac00a9edc09226a169eb21000ef8fa4
Ferry L.H <ferrylinton@gmail.com>
2021-11-14 13:53:49 +0700

    add date picker

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 
 import React from 'react';
-import { FaRegListAlt } from 'react-icons/fa';
+
+import { FaClipboardList } from 'react-icons/fa';
 
 import { TodoForm } from './components/todo-form/todo-form';
 import { TodoItem } from './components/todo-item/todo-item';
+
+
 import './App.scss';
 
 
 function App() {
 
   const [todo, setTodo] = React.useState("");
+  const [startDate, setStartDate] = React.useState(new Date());
+  const [filter, setFilter] = React.useState("all");
+
   const [todos, setTodos] = React.useState([
     {
       task: "This is a sampe todo",
@@ -26,26 +32,35 @@ function App() {
     console.log(event.target.value);
   };
 
+  
+
+  const onChangeDate = (date) => setStartDate(date);
+
+  const onChangeFilter = (event) => {
+    setFilter(event.target.value);
+  }
+
+
   React.useEffect(() => {
     console.log(todo);
+    console.log(startDate);
+    console.log(filter);
   });
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center min-vh-100">
       <div className="todo-container border border-light shadow-sm p-4">
-        <div className="d-flex flex-row justify-content-center">
-          <h1 className="mt-3 mb-5"><FaRegListAlt /> <div style={{ padding: 0, margin: 0, display: "inline-block", textTransform: "uppercase" }}>todo</div></h1>
+        <div className="d-flex flex-row justify-content-center mt-3 mb-5">
+          <div className="fs-1"><FaClipboardList/> <span style={{ textTransform: "uppercase", verticalAlign: "middle" }}>todo</span></div>
         </div>
-        <TodoForm onTodoFormChange={onTodoFormChange} />
-        <div className="my-4 border-bottom"></div>
-        <div className="d-flex flex-row">
+        <TodoForm onTodoFormChange={onTodoFormChange} startDate={startDate} onChangeDate={onChangeDate} />
+        <div className="d-flex flex-row my-4">
           <div className="input-group mx-2">
             <label className="input-group-text text-secondary">Filter</label>
-            <select className="form-select">
+            <select className="form-select" value={filter} onChange={onChangeFilter}>
               <option value="all">All</option>
               <option value="completed">Completed</option>
               <option value="active">Active</option>
-              <option value="has-due-date">Has due date</option>
             </select>
           </div>
           <div className="input-group mx-2">
--- src/components/todo-form/todo-form.jsx
+++ src/components/todo-form/todo-form.jsx
@@ -1,18 +1,38 @@
+import React from 'react';
+import DatePicker from "react-datepicker";
 import { Input, Button } from 'reactstrap';
-import { FaCalendarAlt } from 'react-icons/fa';
+import { FaCalendarAlt, FaSave } from 'react-icons/fa';
 
-export const TodoForm = function (props) {
-  return (
-    <div className="d-flex flex-row p-2 border border-light shadow-sm">
-      <Input
-        id="addNewTxt"
-        name="addNewTxt"
-        placeholder="Add new ..."
-        type="text"
-        className="border-0" 
-        onChange={props.onTodoFormChange}/>
-      <div className="d-inline-block btn text-primary"><FaCalendarAlt /></div>
-      <Button color="primary">ADD</Button>
+import "react-datepicker/dist/react-datepicker.css";
+
+export const TodoForm = function ({ startDate, onChangeDate, onTodoFormChange }) {
+
+  const ExampleCustomInput = React.forwardRef(({ value, onClick }, ref) => (
+    <div className="btn text-primary" onClick={onClick} ref={ref}>
+      <FaCalendarAlt/> <span className="align-middle">{value}</span>
     </div>
+  ));
+
+  return (
+      <div className="d-flex p-2 border border-light shadow-sm">
+        <div className="flex-grow-1">
+        <Input
+          id="addNewTxt"
+          name="addNewTxt"
+          placeholder="Add new ..."
+          type="text"
+          className="border-0"
+          onChange={onTodoFormChange} />
+        </div>
+        <div>
+        <DatePicker
+          selected={startDate}
+          onChange={onChangeDate}
+          minDate={new Date()}
+          customInput={<ExampleCustomInput />} 
+          showDisabledMonthNavigation />
+        </div>
+        <Button color="primary"><FaSave /> <span className="align-middle">ADD</span></Button>
+      </div>
   )
 }
